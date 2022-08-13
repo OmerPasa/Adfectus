ec9e9dbacfd76bb3e1b5f6fdbb511d20742f02d6
IsE333 <77530401+IsE333@users.noreply.github.com>
2022-08-14 00:07:50 +0300

    beat sayma

--- Assets/Scripts/ZamanVeBeat.cs
+++ Assets/Scripts/ZamanVeBeat.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class ZamanVeBeat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
     {
+        Zaman.tick(Time.deltaTime * 1000);
+        Debug.Log(Zaman.gecenSure / 1000);
+        Debug.Log(Zaman.beat);
 
     }
+}
+
+public static class Zaman
+{
+    public static float gecenSure = 0;
+    public static int bpm = 120;
+    public static int beat = 1;
+    static int beatDuration = 60000 / bpm;
+
+
+    public static void baslat(float time)
+    {
+        gecenSure += time;
+    }
+
+    public static void tick(float time)
+    {
+        gecenSure += time;
+        beat = (int)(gecenSure / beatDuration) + 1;
+    }
+
 }
