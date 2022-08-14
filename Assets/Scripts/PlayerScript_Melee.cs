ec98c121dcc60ea92c88c4fca80d2b1bc5593e13
Ömer Faruk Çalışkan <omerfa33@gmail.com>
2022-08-14 16:24:14 +0300

    many little changes but none is works?

--- Assets/Scripts/PlayerScript_Melee.cs
+++ Assets/Scripts/PlayerScript_Melee.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,49 +7,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System;
-public class PlayerScript : MonoBehaviour
+using UnityEngine.SceneManagement;
+
+public class PlayerScript_Melee : MonoBehaviour
 {
-    public ParticleSystem dust;
     [SerializeField]
     private float runSpeed;
 
     [SerializeField]
     public Transform groundCheck;
-
+    [SerializeField]
+    private float jumpForce;
     private Animator animator;
+    Collider2D col2D;
     private string currentAnimaton;
     private float xAxis;
     private float yAxis;
     private Rigidbody2D rb2d;
     private bool isJumpPressed;
-    [SerializeField]
-    private float jumpForce;
-    private int groundMask;
     private bool isGrounded;
     private bool isAttackPressed;
     private bool isAttacking;
     private bool isntDead;
-    private bool DustActive;
     private bool TakingDamage;
-    AudioSource AfterFiringMusic;
+    private bool ControlPressedLeft;
+    private bool ControlPressedRight;
+    [SerializeField]
+    AudioSource StabMusic;
+    [SerializeField]
+    AudioSource DodgeMusic;
+    [SerializeField]
+    AudioSource DamageMusic;
     public AudioSource BackGroundM;
     public bool isFacingLeft = false;
-    public Transform firePoint;
-    public GameObject BulletPre;
-
     [SerializeField]
-    private float attackDelay;
+    public Transform Damage_Box;
+    public float attackRange;
+    public LayerMask Enemies;
+    [SerializeField]
+    private float attackDelay = 0f;
     private float damageDelay;
-    public int maxHealth = 10;
     public int Playerhealth;
-    public healthbar_control healthbar;
-    public MainMenu mainMenu;
+    public int Playerblood = 0;
+    public int damage = 1;
+    private Vector3 dashVc3;
 
     //Animation States
-    const string PLAYER_IDLE = "Player_Idle_Gun";
-    const string PLAYER_RUN = "Player_Movement_Gun";
+    const string PLAYER_IDLE = "Player_Idle";
+    const string PLAYER_DODGEBACKWARD = "Player_DodgeBackward";
+    const string PLAYER_DODGEFORWARD = "Player_DodgeForward";
+    const string PLAYER_RUN = "Player_Run";
     const string PLAYER_JUMP = "Player_Jump_Gun";
-    const string PLAYER_ATTACK = "Player_Movement_Firing";
+    const string PLAYER_ATTACK = "Player_Attack";
     const string PLAYER_AIR_ATTACK = "Player_Jump_Firing";
     const string PLAYER_DEATH = "Player_Death";
     const string PLAYER_TAKEDAMAGE = "Player_TakeDamage";
@@ -61,34 +71,44 @@ public class PlayerScript : MonoBehaviour
         isntDead = true;
         rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        AfterFiringMusic = GetComponent<AudioSource>();
-        BackGroundM = GetComponent<AudioSource>();
-        Playerhealth = maxHealth;
-        healthbar.SetMaxHealth(maxHealth);
-        
+        //AudioSource StabMusic = GameObject.Find("StabMusic").GetComponent<AudioSource>();
+        //AudioSource DamageMusic = GameObject.Find("DamageMusic").GetComponent<AudioSource>();
+        //AudioSource DodgeMusic = GameObject.Find("DodgeMusic").GetComponent<AudioSource>();
+        //AudioSource BackGroundM = GameObject.Find("BackGroundMusic").GetComponent<AudioSource>();
     }
     void Update()
     {
+
         if (Playerhealth <= 0)
         {
             isntDead = false;
             ChangeAnimationState(PLAYER_DEATH);
             Invoke("Die", 2f);
+            SceneManager.LoadScene(0);
+
+
         }
         //Checking for inputs
         xAxis = Input.GetAxisRaw("Horizontal");
         
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
+            if (!isAttacking && !TakingDamage)
+            {
             rb2d.velocity = new Vector2(runSpeed, rb2d.velocity.y);
             transform.localScale = new Vector3(1, 1, 1);
             isFacingLeft = false;
+            }
+
         }
         else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
+            if (!isAttacking && !TakingDamage)
+            {
             rb2d.velocity = new Vector2(-runSpeed, rb2d.velocity.y);
             transform.localScale = new Vector3(-1, 1, 1);
             isFacingLeft = true;
+            }
         }else if (isGrounded)
         {
             rb2d.velocity = new Vector2(0, rb2d.velocity.y);
@@ -100,44 +120,61 @@ public class PlayerScript : MonoBehaviour
         { 
             isJumpPressed = true;
         }
-
-        //space Atatck key pressed?
-        if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKey(KeyCode.LeftControl))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            isAttackPressed = true;
+            ControlPressedLeft = true;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ControlPressedRight = true;
         }
 
-        //Q tuşuna basıcak ve farenin gösterdiği yere 
-        //GRAPLİNG-GUN
+        //space Atatck key pressed?
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            isAttackPressed = true;
+            Debug.Log("attackpressed");
+        }
     }
-
     //=====================================================
     // Physics based time step loop
     //=====================================================
     private void FixedUpdate()
     {
-        ///=================================================
-        //Ground and waterchecks
-        if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")))
+        if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("ground")))
         {
             isGrounded = true;
-            Debug.Log("isGROUNDED_Player");
         }
         else
         {
             isGrounded = false;
         }
 
-        if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Water")))
-        {
-            Playerhealth -= 10;
-        }
-
         //------------------------------------------
-        //animation checks
         
         if (isGrounded && !isAttacking && isntDead && !TakingDamage)
-        {
+        {   
+            if (ControlPressedLeft)
+            {
+                float dash = transform.position.x;
+                transform.position = new Vector3(dash + -10f,transform.position.y,transform.position.z);
+                Debug.Log("DASHED_Player");
+                BackGroundM.volume = 0.6f;
+                DodgeMusic.Play();
+                ChangeAnimationState(PLAYER_DODGEBACKWARD);
+                ControlPressedLeft = false;
+                BackGroundM.volume = 1f;
+            }
+            if (ControlPressedRight)
+            {
+                float dash = transform.position.x;
+                transform.position = new Vector3(dash + 10f,transform.position.y,transform.position.z);
+                BackGroundM.volume = 0.6f;
+                DodgeMusic.Play();
+                ChangeAnimationState(PLAYER_DODGEFORWARD);
+                ControlPressedRight = false;
+                BackGroundM.volume = 1f;
+            }
             if (xAxis != 0)
             {
                 ChangeAnimationState(PLAYER_RUN);
@@ -146,6 +183,7 @@ public class PlayerScript : MonoBehaviour
             {
                 ChangeAnimationState(PLAYER_IDLE);
             }
+            
         }
 
 
@@ -155,9 +193,7 @@ public class PlayerScript : MonoBehaviour
             rb2d.AddForce(new Vector2(0, jumpForce));
             isJumpPressed = false;
             ChangeAnimationState(PLAYER_JUMP);
-            
         }
-
         //attack
         if (isAttackPressed)
         {
@@ -166,44 +202,42 @@ public class PlayerScript : MonoBehaviour
             if (!isAttacking)
             {
                 isAttacking = true;
-                GameObject B = Instantiate(BulletPre,firePoint.position,firePoint.rotation);
-                B.GetComponent<BulletScriptt>().StartShooting(isFacingLeft);
-                AfterFiringMusic.Play();
+                Collider2D[] enemiesInRange = Physics2D.OverlapCircleAll(Damage_Box.position, attackRange, Enemies);
+                //for giving every one of enemies damage.
+                for (int i = 0; i < enemiesInRange.Length; i++)
+                {
+                ///enemiesInRange[i].GetComponent<CommonEnemy>().TakeDamage(damage);
+                Debug.Log("damage givenby player");
+                }
+                    BackGroundM.volume = 0.6f;
+                    StabMusic.Play();
                 if(isGrounded)
                 {
                     ChangeAnimationState(PLAYER_ATTACK);
-                    BackGroundM.volume = 0.4f;
-                    AfterFiringMusic.Play();
-                    CinemachineShake.Instance.ShakeCamera(1f, .5f);
+
                 }
                 else
                 {
                     ChangeAnimationState(PLAYER_AIR_ATTACK);
-                    BackGroundM.volume = 0.4f;
                 }
+                attackDelay = animator.GetCurrentAnimatorStateInfo(0).length + 0.2f;
                 Invoke("AttackComplete", attackDelay);
             }
         }
-        //Debug.Log("SPEED FOR PLAYER" +  rb2d.velocity.magnitude);
-
-        if (rb2d.velocity.magnitude > 1)
-        {
-            DustActive = true;
-        }else
-        {
-            DustActive = false;
-        }
-        if (DustActive)
-        {
-            dust.Play();
-        }
     }
+    /*IEnumerator AttackStart()
+    {
+        Damage_Box.SetActive(true);
+        yield return new WaitForSeconds(.4f);
+        Damage_Box.SetActive(false);
+        isAttacking = false;
+    }*/
+
     void AttackComplete()
     {
         isAttacking = false;
         BackGroundM.volume = 1f;
         Debug.Log("ATTACKCOMPLETE");
-        CinemachineShake.Instance.ShakeCamera(0f, 0f);
     }
     public void Die()
     {
@@ -211,25 +245,24 @@ public class PlayerScript : MonoBehaviour
     }
     public void PlayerTakeDamage(int damage)
     {
+        BackGroundM.volume = 0.6f;
         TakingDamage = true;
         Playerhealth -= damage;
-        healthbar.SetHealth(Playerhealth);
-        Debug.Log("damageTaken");
+        Debug.Log("damageTakenbyplayer");
+        DamageMusic.Play();
         ChangeAnimationState(PLAYER_TAKEDAMAGE);
-        Debug.Log("ANİMATİON CHANGED TO TAKEDAMAGE!!!!!!!!");
-        damageDelay = animator.GetCurrentAnimatorStateInfo(0).length;
+        damageDelay = animator.GetCurrentAnimatorStateInfo(0).length + 1f;
         Invoke("DamageDelayComplete", damageDelay);
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(Damage_Box.position, attackRange);
     }
     void DamageDelayComplete()
     {
+        BackGroundM.volume = 1f;
         TakingDamage = false;
-    }
-    void OnCollisionEnter2D(Collision2D water) 
-    {
-        if (water.gameObject.tag == "Water")
-        {
-            mainMenu.GameIsOver();
-        }
     }
 
     //=====================================================
@@ -241,10 +274,6 @@ public class PlayerScript : MonoBehaviour
 
         animator.Play(newAnimation);
         currentAnimaton = newAnimation;
-    }
-    void CreateDust()
-    {
-        dust.Play();
     }
 
 }
