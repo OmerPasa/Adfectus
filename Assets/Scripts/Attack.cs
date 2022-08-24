c4c2291c26c5b153755e98bd2502573be5cbf1b9
IsE333 <77530401+IsE333@users.noreply.github.com>
2022-08-25 01:24:29 +0300

    unnecessary parts are removed

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -5,37 +5,16 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     public AttackData data;
-    //public int startBeatQuarter;
-    /*public int type;
-    public int beat;
-    public int beatQuarter;
-    public int duration;
-    public int g1;
-    public GameObject handParcasiData1;*/
-
 
     void Update()
     {
-        /*if (TimeB.beat == eklemBeat[index] && TimeB.beatQuarter == eklemBeatQuarter[index])
-        {
-
-        }*/
-
-
         if (data == null)
         {
             return;
         }
 
-        /*if (data.duration.CounterEQ < TimeB.Counter_Q)
-        {
-            return;
-        }*/
-
-        Debug.Log("cq: " + TimeB.Counter_Q + ", data CQ" + data.duration.CounterEQ + " ");
         if (data.duration.CounterEQ == TimeB.Counter_Q)
         {
-            Debug.Log(data.type + " end");
             endAttack();
         }
     }
