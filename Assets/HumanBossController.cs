4361253119ceb5c3cb62e4c0a2b7721534410297
Ömer Faruk Çalışkan <omerfa33@gmail.com>
2023-10-06 15:52:44 +0300

    so the mailn problem with middle attack
    was the placement of the long attackbeneith  it  , taking long attck to ypper side solved it  but now it does n't working as well. for some reason if  inside of the middle attack doesn't trigers.

--- Assets/HumanBossController.cs
+++ Assets/HumanBossController.cs
@@ -53,8 +53,8 @@ public class HumanBossController : MonoBehaviour
     public int health = 4;
     public float damage;
     int Count;
-    [SerializeField] public float timeBtwAttack;
-    [SerializeField] public float startTimeBtwAttack;
+    [SerializeField] public float timeBtw_shortAttack;
+    [SerializeField] public float startTimeBtw_shortAttack;
     [SerializeField] public float timeBtw_midAttack;
     [SerializeField] public float startTimeBtw_midAttack;
     [SerializeField] public float timeBtw_longAttack;
@@ -342,27 +342,31 @@ public class HumanBossAttackInitiater : HumanBossBaseState
 {
     public override void EnterState(HumanBossController boss)
     {
+        if (boss.isAttackingMedium == false && boss.isAttackingShort == false)
+        {
 
-        //boss.timeBtwAttack ı mı silsek ?
-        Deb.ug("İnitiating Attack");
-        if (Vector3.Distance(boss.transform.position, boss.character.transform.position) <= boss.longRange && boss.timeBtw_longAttack <= 0 && boss.canInstantiate == true)
-        {
-            boss.SwitchState(boss.longState);
-            Debug.Log("long attack");
-        }
-        if (Vector3.Distance(boss.transform.position, boss.character.transform.position) <= boss.meleeRange && boss.timeBtwAttack <= 0 && boss.isAttackingMedium == false)
-        {
-            boss.SwitchState(boss.meleeState);
-            Debug.Log("melee attack");
-        }
-        if (Vector3.Distance(boss.transform.position, boss.character.transform.position) <= boss.mediumRange && boss.timeBtw_midAttack <= 0 && boss.isAttackingShort == false)
-        {
-            boss.SwitchState(boss.mediumState);
-            Debug.Log("medium attack");
-        }
-        else
-        {
-            boss.SwitchState(boss.runningState);
+            //boss.timeBtwAttack ı mı silsek ?
+            Deb.ug("İnitiating Attack");
+            if (Vector3.Distance(boss.transform.position, boss.character.transform.position) <= boss.meleeRange && boss.timeBtw_shortAttack <= 0 && boss.isAttackingMedium == false)
+            {
+                boss.SwitchState(boss.meleeState);
+                Debug.Log("melee attack");
+            }
+            else if (Vector3.Distance(boss.transform.position, boss.character.transform.position) <= boss.mediumRange && boss.timeBtw_midAttack <= 0 && boss.isAttackingShort == false)
+            {
+                boss.SwitchState(boss.mediumState);
+                Debug.Log("medium attack");
+            }
+            else if (Vector3.Distance(boss.transform.position, boss.character.transform.position) <= boss.longRange && boss.timeBtw_longAttack <= 0 && boss.canInstantiate == true)
+            {
+                boss.SwitchState(boss.longState);
+                Debug.Log("long attack");
+            }
+            else
+            {
+                boss.SwitchState(boss.runningState);
+            }
+
         }
     }
 
@@ -398,6 +402,7 @@ public class HumanBossRunState : HumanBossBaseState
             }
             Vector3 karPos = boss.character.transform.position;
             Vector3 pos = boss.transform.position;
+            boss.timeBtw_shortAttack -= Time.deltaTime;
             boss.timeBtw_midAttack -= Time.deltaTime;
             boss.timeBtw_longAttack -= Time.deltaTime;
             if (Mathf.Abs(karPos.x - pos.x) < boss.viewRange)
@@ -476,7 +481,7 @@ public class HumanBossMeleeState : HumanBossBaseState
             if (playerController != null)
             {
                 playerController.PlayerTakeDamage(boss.damage); // is not getting called
-                boss.timeBtwAttack = boss.startTimeBtwAttack;
+                boss.timeBtw_shortAttack = boss.startTimeBtw_shortAttack;
                 boss.SwitchState(boss.runningState);
                 boss.movementSpeed = 2f;
                 boss.Invoke("AttackComplete", boss.damageDelay);
@@ -484,13 +489,13 @@ public class HumanBossMeleeState : HumanBossBaseState
             }
             if (playerController = null)
             {
-                boss.timeBtwAttack -= Time.deltaTime;
+                boss.timeBtw_shortAttack -= Time.deltaTime;
                 boss.isAttackingShort = false;
                 boss.SwitchState(boss.runningState);
                 Debug.Log("Player is null!");
             }
 
-            boss.timeBtwAttack -= Time.deltaTime;
+            boss.timeBtw_shortAttack -= Time.deltaTime;
             boss.SwitchState(boss.runningState);
 
 
@@ -509,8 +514,10 @@ public class HumanBossMediumState : HumanBossBaseState
 
     public override void EnterState(HumanBossController boss)
     {
+        Deb.ug("staring to fire fireballs.");
         // Reset the current part to 1 when entering the state
         currentPart = 1;
+        boss.timeBtw_midAttack = boss.startTimeBtw_midAttack;
         boss.isAttackingMedium = true;
     }
 
@@ -527,12 +534,13 @@ public class HumanBossMediumState : HumanBossBaseState
             return;
         }
 
+        Deb.ug("firing fireballs outside.");
         // Increment the timer
-        timer += Time.deltaTime;
 
         // Check if the delay between fires has passed
         if (timer >= delayBetweenFires && boss.character != null)
         {
+            Deb.ug("firing fireballs inside.");
             timer = 0f;  // Reset the timer
             // Perform the appropriate action for the current part of the attack
             switch (currentPart)
@@ -560,6 +568,7 @@ public class HumanBossMediumState : HumanBossBaseState
                     break;
             }
 
+            timer += Time.deltaTime;
             // Increase the current part for the next update
             currentPart++;
         }
@@ -567,12 +576,13 @@ public class HumanBossMediumState : HumanBossBaseState
 
     private void CreateFire(Vector3 position, HumanBossController boss)
     {
+        Deb.ug("Creatingfire");
         // Instantiate the fire effect at the specified position
         GameObject fire = GameObject.Instantiate(boss.fireObject, position, Quaternion.identity);
 
         // Destroy the fire effect after the specified duration
         GameObject.Destroy(fire, fireDuration);
-        boss.timeBtwAttack = boss.startTimeBtwAttack;
+        boss.timeBtw_shortAttack = boss.startTimeBtw_shortAttack;
     }
 
     public override void OnCollisionEnter(HumanBossController boss, Collision2D collision)
